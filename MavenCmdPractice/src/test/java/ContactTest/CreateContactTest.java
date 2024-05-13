09a50701de3b27b33dbacb050196670344837aef
Vinay krishna <37305617+vinaykrishnahv@users.noreply.github.com>
2024-05-13 11:08:57 +0530

    first push

---
+++ MavenCmdPractice/src/test/java/ContactTest/CreateContactTest.java
@@ -0,0 +1,32 @@
+package ContactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	
+	
+	@Test
+	public void createContactTest() {
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("Contact Created and Executed");
+	}
+
+	@Test
+	public void createContactWithDateTest() {
+		System.out.println("Contact Date Created and Executed");
+	}
+
+	@Test
+	public void createContactWithOrgTest() {
+		System.out.println("Contact with Org Created and Executed");
+	}
+}
---
+++ MavenCmdPractice/src/test/java/OrgTest/CreateOrgTest.java
@@ -0,0 +1,21 @@
+ package OrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrgTest() {
+		System.out.println("Org Created and Executed");
+	}
+
+	@Test
+	public void createOrgWithPhoneTest() {
+		System.out.println("Org Phone Created and Executed");
+	}
+
+	@Test
+	public void createOrgWithIndTest() {
+		System.out.println("Org with Industry Created and Executed");
+	}
+}
