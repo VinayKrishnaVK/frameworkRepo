6f75b3661e2b034ecba6a69dc300ffa8716b147c
Vinay krishna <37305617+vinaykrishnahv@users.noreply.github.com>
2024-05-13 12:09:58 +0530

    parameters added to OrgTest

--- MavenCmdPractice/src/test/java/OrgTest/CreateOrgTest.java
+++ MavenCmdPractice/src/test/java/OrgTest/CreateOrgTest.java
@@ -4,8 +4,16 @@ import org.testng.annotations.Test;
 
 public class CreateOrgTest {
 
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
 	@Test
 	public void createOrgTest() {
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("Org Created and Executed");
 	}
 
